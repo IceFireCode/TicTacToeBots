dcc29a06458b5a5009bd910f5577ac9f43a35899
Paul van Cappelle <paul.vancappelle@ns.nl>
2024-10-07 16:14:58 +0200

    Add a newline after the greeting

--- src/main/java/org/example/Main.kt
+++ src/main/java/org/example/Main.kt
@@ -3,7 +3,7 @@ package org.example
 import org.example.game.TicTacToeBots
 
 fun main(args: Array<String>) {
-    println("Hello TicTacToeBots player!")
+    println("Hello TicTacToeBots player!\n")
 
     TicTacToeBots().start()
 }
