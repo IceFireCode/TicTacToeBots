93a50971bc9d2003e5bbe52506268607804e2f87
Paul van Cappelle <paul.vancappelle@ns.nl>
2024-10-07 16:13:08 +0200

    Make the test for updating the current player succeed

--- src/main/java/org/example/game/TicTacToeBots.kt
+++ src/main/java/org/example/game/TicTacToeBots.kt
@@ -16,8 +16,12 @@ class TicTacToeBots(
              gameUI.showCurrentPlayer(currentPlayer)
             board.makeMove(currentPlayer)
              gameUI.showBoard(board)
+            currentPlayer = swapPlayer(currentPlayer = currentPlayer)
         }
 
         gameUI.showEndResult(board = board)
     }
+
+    // TODO @VisibleForTesting and make private
+    fun swapPlayer(currentPlayer: Player): Player = if (currentPlayer == Player.ONE) Player.TWO else Player.ONE
 }
