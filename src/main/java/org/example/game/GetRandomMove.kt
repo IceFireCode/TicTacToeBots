5a04f5695c149edf091c613ace69b4dba89f1c92
Paul van Cappelle <paul.vancappelle@ns.nl>
2024-10-07 15:41:43 +0200

    Make test for scenario in which the first random move cannot be executed succeed

--- src/main/java/org/example/game/GetRandomMove.kt
+++ src/main/java/org/example/game/GetRandomMove.kt
@@ -14,7 +14,8 @@ class FakeGetRandomMove(val values: List<Pair<Int, Int>>): GetRandomMove {
     private var counter = 0
 
     override operator fun invoke(): Pair<Int, Int> {
-        return values[counter]
+        val result = values[counter]
         counter++
+        return result
     }
 }
