dbc1ee30c79f2011c96a9805c42d2f60f62ddf22
Paul van Cappelle <paul.vancappelle@ns.nl>
2024-10-07 12:31:56 +0200

    Make initialize board test succeed using Player

--- src/main/java/org/example/game/TicTacToeBots.kt
+++ src/main/java/org/example/game/TicTacToeBots.kt
@@ -1,8 +1,10 @@
 package org.example.game
 
+import org.example.game.model.Player
+
 class TicTacToeBots {
 
-    fun initializeBoard(): List<String?> {
+    fun initializeBoard(): List<Player?> {
         return listOf(null, null, null, null, null, null, null, null, null)
     }
 
