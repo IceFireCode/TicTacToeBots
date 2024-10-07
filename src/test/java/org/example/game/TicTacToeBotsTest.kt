c18ddbda5832857b59bc3c164025386095c1eb94
Paul van Cappelle <paul.vancappelle@ns.nl>
2024-10-07 16:12:07 +0200

    Write a test for updating the current player after each move

--- src/test/java/org/example/game/TicTacToeBotsTest.kt
+++ src/test/java/org/example/game/TicTacToeBotsTest.kt
@@ -6,4 +6,29 @@ import org.junit.jupiter.api.Test
 
 class TicTacToeBotsTest {
 
+    @Test
+    fun `Given player one is the current player, swapPlayer returns player two`(){
+        // Given
+        val currentPlayer = Player.ONE
+        val ticTacToeBots = TicTacToeBots()
+
+        // When
+        val result = ticTacToeBots.swapPlayer(currentPlayer)
+
+        // Then
+        assertEquals(Player.TWO, result)
+    }
+
+    @Test
+    fun `Given player two is the current player, swapPlayer returns player one`(){
+        // Given
+        val currentPlayer = Player.TWO
+        val ticTacToeBots = TicTacToeBots()
+
+        // When
+        val result = ticTacToeBots.swapPlayer(currentPlayer)
+
+        // Then
+        assertEquals(Player.ONE, result)
+    }
 }
