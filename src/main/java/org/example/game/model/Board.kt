75280c13486445738e77beeabc2d2ad2f82f6c32
Paul van Cappelle <paul.vancappelle@ns.nl>
2024-10-07 13:08:36 +0200

    Make the Board tests succeed

--- src/main/java/org/example/game/model/Board.kt
+++ src/main/java/org/example/game/model/Board.kt
@@ -1,9 +1,9 @@
 package org.example.game.model
 
 data class Board(
-    val initialBoard: Board? = null, // this param allows for setting the start state in tests
+    val initialBoard: List<List<Player?>>? = null, // this param allows for setting the start state in tests
 ) {
-    val fields: List<List<Player?>> = initialBoard?.fields ?: listOf(
+    val fields: List<List<Player?>> = initialBoard ?: listOf(
         listOf(null, null, null),
         listOf(null, null, null),
         listOf(null, null, null),
